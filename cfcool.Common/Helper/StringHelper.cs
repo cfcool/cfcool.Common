4d5be7b4f4b8c06b97186f9123506f4cbb6544a9
Administrator <cfcool@163.com>
2018-12-16 00:17:51 +0800

    初始化项目

---
+++ cfcool.Common/Helper/StringHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cfcool.Common.Helper
+{
+    public static class StringHelper
+    {
+        public static string FirstCharacterUpper(string src)
+        {
+            return src;
+        }
+    }
+}
